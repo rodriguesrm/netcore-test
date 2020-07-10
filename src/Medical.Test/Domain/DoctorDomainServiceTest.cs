c697d51d069134f239bfa81115a3330083312b1a
Rodrigo Rodrigues <rodriguesrm@gmail.com>
2020-07-10 11:14:33 -0300

    Inicio dos testes unitarios

---
+++ src/Medical.Test/Domain/DoctorDomainServiceTest.cs
@@ -0,0 +1,136 @@
+﻿using Medical.Domain.Entities;
+using Medical.Domain.Services;
+using NSubstitute;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using Xunit;
+
+namespace Medical.Test.Domain
+{
+
+    /// <summary>
+    /// Doctor domain service tests
+    /// </summary>
+    public class DoctorDomainServiceTest
+    {
+
+        private Doctor GetDoctor()
+            => new Doctor()
+            {
+                FirstName = "OLIVER",
+                LastName = "QUEEN",
+                Crm = "10001/2001"
+            };
+
+        /// <summary>
+        /// Doctor add test
+        /// </summary>
+        [Fact]
+        public void AddDoctorTest()
+        {
+
+            Doctor doctor = GetDoctor();
+            IDoctorRepository repository = Substitute.For<IDoctorRepository>();
+            repository.AddAsync(Arg.Any<Doctor>(), Arg.Any<CancellationToken>()).Returns(doctor);
+
+            DoctorDomainService service = new DoctorDomainService(repository);
+            Doctor result = service.AddAsync(doctor, default).Result;
+
+            Assert.NotNull(result);
+
+        }
+
+        /// <summary>
+        /// Doctor update test
+        /// </summary>
+        [Fact]
+        public void UpdateDoctorTest()
+        {
+
+            Doctor doctor = GetDoctor();
+            IDoctorRepository repository = Substitute.For<IDoctorRepository>();
+            repository.Update(Arg.Any<Doctor>()).Returns(doctor);
+
+            DoctorDomainService service = new DoctorDomainService(repository);
+            Doctor result = service.Update(doctor);
+
+            Assert.NotNull(result);
+
+        }
+
+        /// <summary>
+        /// Doctor get by id test
+        /// </summary>
+        [Fact]
+        public void GetByIdDoctorTest()
+        {
+
+            Doctor doctorToFind = GetDoctor();
+
+            IEnumerable<Doctor> doctors = new List<Doctor>()
+            {
+                new Doctor() { FirstName = "CLARK", LastName = "KENT", Crm = "K0001/2000"  },
+                doctorToFind,
+                new Doctor() { FirstName = "NATASHA", LastName = "ROMANOFF", Crm = "K0003/2000"  }
+            };
+
+            IDoctorRepository repository = Substitute.For<IDoctorRepository>();
+            repository.GetByIdAsync(Arg.Any<Guid>(), Arg.Any<CancellationToken>()).Returns(x => doctors.FirstOrDefault(d => d.Id == new Guid(x[0].ToString())));
+
+            DoctorDomainService service = new DoctorDomainService(repository);
+            Doctor result = service.GetByIdAsync(doctorToFind.Id.Value, default).Result;
+
+            Assert.NotNull(result);
+            Assert.Equal(doctorToFind.FirstName, result.FirstName);
+            Assert.Equal(doctorToFind.LastName, result.LastName);
+            Assert.Equal(doctorToFind.Crm, result.Crm);
+
+        }
+
+        /// <summary>
+        /// Doctor get all test
+        /// </summary>
+        [Fact]
+        public void GetByAllDoctorTest()
+        {
+
+            Doctor doctorToFind = GetDoctor();
+
+            IQueryable<Doctor> doctors = (new List<Doctor>()
+            {
+                new Doctor() { FirstName = "CLARK", LastName = "KENT", Crm = "K0001/2000"  },
+                new Doctor() { FirstName = "NATASHA", LastName = "ROMANOFF", Crm = "K0003/2000"  }
+            }).AsQueryable();
+
+            IDoctorRepository repository = Substitute.For<IDoctorRepository>();
+            repository.GetAllAsync(Arg.Any<CancellationToken>()).Returns(doctors);
+
+            DoctorDomainService service = new DoctorDomainService(repository);
+            IEnumerable<Doctor> result = service.GetAllAsync(default).Result.ToList();
+
+            Assert.NotNull(result);
+            Assert.Equal(doctors.Count(), result.Count());
+
+        }
+
+        /// <summary>
+        /// Doctor delete test
+        /// </summary>
+        [Fact]
+        public void DeleteDoctorTest()
+        {
+
+            IDoctorRepository repository = Substitute.For<IDoctorRepository>();
+
+            repository.Delete(Arg.Any<Doctor>());
+
+            DoctorDomainService service = new DoctorDomainService(repository);
+            service.Delete(GetDoctor());
+
+        }
+
+    }
+
+}
---
+++ src/Medical.Test/Domain/PatientDomainServiceTest.cs
@@ -0,0 +1,136 @@
+﻿using Medical.Domain.Entities;
+using Medical.Domain.Services;
+using NSubstitute;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using Xunit;
+
+namespace Medical.Test.Domain
+{
+
+    /// <summary>
+    /// Patient domain service tests
+    /// </summary>
+    public class PatientDomainServiceTest
+    {
+
+        private Patient GetPatient()
+            => new Patient()
+            {
+                FirstName = "OLIVER",
+                LastName = "QUEEN",
+                Cpf = "26725871402"
+            };
+
+        /// <summary>
+        /// Patient add test
+        /// </summary>
+        [Fact]
+        public void AddPatientTest()
+        {
+
+            Patient Patient = GetPatient();
+            IPatientRepository repository = Substitute.For<IPatientRepository>();
+            repository.AddAsync(Arg.Any<Patient>(), Arg.Any<CancellationToken>()).Returns(Patient);
+
+            PatientDomainService service = new PatientDomainService(repository);
+            Patient result = service.AddAsync(Patient, default).Result;
+
+            Assert.NotNull(result);
+
+        }
+
+        /// <summary>
+        /// Patient update test
+        /// </summary>
+        [Fact]
+        public void UpdatePatientTest()
+        {
+
+            Patient Patient = GetPatient();
+            IPatientRepository repository = Substitute.For<IPatientRepository>();
+            repository.Update(Arg.Any<Patient>()).Returns(Patient);
+
+            PatientDomainService service = new PatientDomainService(repository);
+            Patient result = service.Update(Patient);
+
+            Assert.NotNull(result);
+
+        }
+
+        /// <summary>
+        /// Patient get by id test
+        /// </summary>
+        [Fact]
+        public void GetByIdPatientTest()
+        {
+
+            Patient patientToFind = GetPatient();
+
+            IEnumerable<Patient> Patients = new List<Patient>()
+            {
+                new Patient() { FirstName = "NATASHA", LastName = "ROMANOFF", Cpf = "K0003/2000" },
+                patientToFind,
+                new Patient() { FirstName = "NATASHA", LastName = "ROMANOFF", Cpf = "69830658163"  }
+            };
+
+            IPatientRepository repository = Substitute.For<IPatientRepository>();
+            repository.GetByIdAsync(Arg.Any<Guid>(), Arg.Any<CancellationToken>()).Returns(x => Patients.FirstOrDefault(d => d.Id == new Guid(x[0].ToString())));
+
+            PatientDomainService service = new PatientDomainService(repository);
+            Patient result = service.GetByIdAsync(patientToFind.Id.Value, default).Result;
+
+            Assert.NotNull(result);
+            Assert.Equal(patientToFind.FirstName, result.FirstName);
+            Assert.Equal(patientToFind.LastName, result.LastName);
+            Assert.Equal(patientToFind.Cpf, result.Cpf);
+
+        }
+
+        /// <summary>
+        /// Patient get all test
+        /// </summary>
+        [Fact]
+        public void GetByAllPatientTest()
+        {
+
+            Patient patientToFind = GetPatient();
+
+            IQueryable<Patient> Patients = (new List<Patient>()
+            {
+                new Patient() { FirstName = "CLARK", LastName = "KENT", Cpf = "95354366437"  },
+                new Patient() { FirstName = "NATASHA", LastName = "ROMANOFF", Cpf = "69830658163"  }
+            }).AsQueryable();
+
+            IPatientRepository repository = Substitute.For<IPatientRepository>();
+            repository.GetAllAsync(Arg.Any<CancellationToken>()).Returns(Patients);
+
+            PatientDomainService service = new PatientDomainService(repository);
+            IEnumerable<Patient> result = service.GetAllAsync(default).Result.ToList();
+
+            Assert.NotNull(result);
+            Assert.Equal(Patients.Count(), result.Count());
+
+        }
+
+        /// <summary>
+        /// Patient delete test
+        /// </summary>
+        [Fact]
+        public void DeletePatientTest()
+        {
+
+            IPatientRepository repository = Substitute.For<IPatientRepository>();
+
+            repository.Delete(Arg.Any<Patient>());
+
+            PatientDomainService service = new PatientDomainService(repository);
+            service.Delete(GetPatient());
+
+        }
+
+    }
+
+}
